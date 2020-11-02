afc05aa8da742e5b7d590f36420fcc07f6c4df4c
XimmersiveX <rwalkos@gmail.com>
2020-11-02 03:04:10 -0600

    Lots of changes

--- js/init.js
+++ js/init.js
@@ -17,15 +17,19 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, {
     coverTrigger: false,
-    hover: true,
   });
 });
-
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.materialboxed');
   var instances = M.Materialbox.init(elems, {});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.scrollspy');
+  var instances = M.ScrollSpy.init(elems, {});
+});
+
 //(function($){
 //  $(function(){
 
