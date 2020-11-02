36ce28d5b0ff890dcc3e6df2cf953c17e8b5711a
XimmersiveX <rwalkos@gmail.com>
2020-11-01 22:56:17 -0600

    init

---
+++ js/init.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems,{});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, {
+    coverTrigger: false,
+    hover: true,
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, {});
+});
+//(function($){
+//  $(function(){
+
+    
+//    $('.sidenav').sidenav();
+//    $('.parallax').parallax();
+//    $(".dropdown-trigger").dropdown();
+
+//  }); // end of document ready
+//})(jQuery); // end of jQuery name space
+
+//$(document).ready(function(){
+//  $('.materialboxed').materialbox();
+//});
